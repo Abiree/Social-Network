cd780cacaf55bfaf153ccf6b37c77fcd40d3c591
Abiree <abireghallabii@gmail.com>
2021-05-25 22:20:02 +0100

    UnitTest

--- server.js
+++ server.js
@@ -1,48 +1,21 @@
-const express = require('express');
+
 //importation de mongoose
 const mongoose = require('mongoose');
-//importation de body-parser
-const bodyParser = require('body-parser');
-//importation de cookieParser
-const cookieParser = require('cookie-parser');
+
 //importation de socketio
 const socketio = require("socket.io");
 //importation et utilisation de dotenv
 require('dotenv').config({path:'./config/.env'});
-//middleware de l'authentification
-const {checkUser,requireAuth} = require('./middleware/auth.middleware');
+
 //middleware de socket
 const {connection} = require('./middleware/socket.middleware');
-//importation des routes
-const users = require('./routes/api/users.routes');
-const posts = require('./routes/api/posts.routes');
-//const chatRoomRouter = require("./routes/api/chatRoom.routes");
-//Autorisation les requetes pour CLIENTS_URL
-const cors = require('cors');
-const corsOptions = {
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-  }
- 
-// init express
-const app = express();
+//import app.js
+const app =require("./app");
+
 // init our socket 
 const httpServer = require("http").createServer(app);
 const io = socketio(httpServer).sockets;
-//autorisation des requetes
-app.use(cors(corsOptions));
-// bodyparser middleware
-app.use(bodyParser.json());
-app.use(cookieParser());
-//jwt (chaque fois qu'il y a une requet * cad n'import quelle requête on doit vérifier si le user dispose d'un token)
-app.get('/*', checkUser);
-app.get('/jwtid',requireAuth,(req,res)=>{
-    res.status(200).send(res.locals.user._id);
-});
+
 //db config
 const db = require('./config/keys').mongoURI;
 //connect to DB
@@ -54,13 +27,7 @@ mongoose.connect(db,{
 })
 .then(()=>console.log("Connected ..."))
 .catch(err => console.log(err));
-//routes
-app.use('/api/users',users);
-app.use('/api/posts',posts);
-//Access to folder image
-app.use('/uploads/avatar', express.static(process.cwd() + '/uploads/avatar'));
-app.use('/uploads/posts', express.static(process.cwd() + '/uploads/posts'));
-app.use('/default', express.static(process.cwd() + '/default'));
+
 //Socket middleware invocation 
 connection(io);
 /*-----------------------test-----------------------------------
